f6febd61ae157e0471fc61630f987d2476ec666d
MenotiFilho <menotimfilho@gmail.com>
2022-02-22 12:40:00 -0300

    nav scroll fixed

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
 import React from "react";
 import requests from "../utils/requests";
+import { useHorizontalScroll } from "../utils/useSideScroll";
 
 function Nav() {
 	const router = useRouter();
-
+	const scrollRef = useHorizontalScroll();
 	return (
 		<nav className="relative">
-			<div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide overflow-y-hidden">
+			<div
+				ref={scrollRef}
+				className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide overflow-y-hidden overscroll-contain"
+			>
 				{Object.entries(requests).map(([key, { title, url }]) => (
 					<h2
 						className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
@@ -18,7 +22,7 @@ function Nav() {
 					</h2>
 				))}
 			</div>
-			<div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202a] h-10 w-1/12 pointer-events-none" />
+			<div className="visible absolute top-0 left-0 bg-gradient-to-r from-[#06202a] h-10 w-1/12 pointer-events-none 2xl:hidden" />
 			<div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202a] h-10 w-1/12 pointer-events-none" />
 		</nav>
 	);
